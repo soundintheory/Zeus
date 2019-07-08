249722ca11149e991b69a8e8361a3bfd14f32d10
Andy White <andy@soundintheory.co.uk>
2019-07-08 15:41:09 +0100

    Tweaks to support area routing

---
+++ Source/Zeus/Configuration/NamespaceElement.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+
+namespace Zeus.Configuration
+{
+    public class NamespaceElement : ConfigurationElement
+    {
+        [ConfigurationProperty("namespace")]
+        public string Namespace
+        {
+            get { return (string)this["namespace"]; }
+            set { this["namespace"] = value; }
+        }
+    }
+}
---
+++ Source/Zeus/Configuration/RoutingSection.cs
@@ -0,0 +1,15 @@
+﻿using System.Configuration;
+
+namespace Zeus.Configuration
+{
+    public class RoutingSection : ConfigurationSection
+    {
+        [ConfigurationProperty("controllers")]
+        [ConfigurationCollection(typeof(NamespaceElementCollection))]
+        public NamespaceElementCollection Controllers
+        {
+            get { return (NamespaceElementCollection)this["controllers"]; }
+            set { this["controllers"] = value; }
+        }
+    }
+}
