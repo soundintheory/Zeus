9acb32d241b6a13f45349ec35a23bdd3468941a3
Andy White <andy@soundintheory.co.uk>
2021-01-08 22:58:59 +0000

    Add custom file image source with updated property for cache busting

---
+++ Source/Zeus/FileSystem/Images/FileImageSource.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using SoundInTheory.DynamicImage.Sources;
+
+namespace Zeus.FileSystem.Images
+{
+    public class ZeusFileImageSource : FileImageSource
+    {
+        public DateTime Updated
+        {
+            get { return (DateTime)(this["Updated"] ?? default(DateTime)); }
+            set { this["Updated"] = value; }
+        }
+    }
+}
