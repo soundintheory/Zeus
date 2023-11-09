fc0eb34865cf2513b32c37974fe73bf4b906cf6f
Harry Negus <harry@soundintheory.co.uk>
2023-11-09 15:14:07 +0000

    WUN-924 Zeus Safe Nodes (Confirm Move)

--- Source/Zeus.Admin/Plugins/MoveItem/MoveItemTreePlugin.cs
+++ Source/Zeus.Admin/Plugins/MoveItem/MoveItemTreePlugin.cs
@@ -1,4 +1,6 @@
 using Ext.Net;
+using System.Xml.Linq;
+using System;
 
 namespace Zeus.Admin.Plugins.MoveItem
 {
@@ -11,14 +13,34 @@ namespace Zeus.Admin.Plugins.MoveItem
 
 		public override void ModifyTree(TreePanel treePanel, IMainInterface mainInterface)
 		{
-			treePanel.Listeners.MoveNode.Handler = string.Format(@"
-				{0}.showBusy();
+
+			if (!Context.SecurityManager.IsAdmin(Context.Current.WebContext.User))
+			{
+				//useful arguments for the handler :) 
+				//console.info('tree', tree)
+				//console.info('node', node)
+				//console.info('oldParent', oldParent)
+				//console.info('newParent', newParent)
+				//console.info('index', index)
+				treePanel.Listeners.BeforeMoveNode.Handler = @"		
+					if(oldParent.id !== newParent.id){{
+						var confirmed = confirm('Are you sure you want to move this node?');
+						return confirmed;
+					}}
+
+					return true;
+				";
+			}
+			
+            treePanel.Listeners.MoveNode.Handler = string.Format(@"
+				{0}.showBusy();					
 				Ext.net.DirectMethods.Move.MoveNode(node.id, newParent.id, index,
 				{{
 					url: '{1}',
 					success: function() {{ {0}.setStatus({{ text: 'Moved item', iconCls: '', clear: true }}); }}
-				}})",
-				mainInterface.StatusBar.ClientID, Context.AdminManager.GetAdminDefaultUrl());
+				}});
+				",
+					mainInterface.StatusBar.ClientID, Context.AdminManager.GetAdminDefaultUrl());
 		}
 	}
 }
