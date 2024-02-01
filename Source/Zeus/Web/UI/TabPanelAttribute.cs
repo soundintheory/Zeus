4e3b7d0c51addc9f86fa9308d0bf5a3266c6e947
Andy White <andy@soundintheory.co.uk>
2024-02-01 15:14:45 +0000

    Allow multiple TabPanels in the content type editor

--- Source/Zeus/Web/UI/TabPanelAttribute.cs
+++ Source/Zeus/Web/UI/TabPanelAttribute.cs
@@ -8,31 +8,43 @@ namespace Zeus.Web.UI
 	{
 		public string Title { get; set; }
 
-		public TabPanelAttribute(string name, string title, int sortOrder)
+		public string ParentID { get; set; }
+
+        public TabPanelAttribute(string name, string title, int sortOrder)
 			: base(name, sortOrder)
 		{
 			Title = title;
 		}
 
-		public override Control AddTo(Control container)
+        public TabPanelAttribute(string name, string title, int sortOrder, string parentId)
+            : base(name, sortOrder)
+        {
+            Title = title;
+			ParentID = parentId;
+        }
+
+        public override Control AddTo(Control container)
 		{
-			// If the current container doesn't already contain a TabControl, create one now.
-			CustomTabPanel tabControl = container.FindControl("TabControl") as CustomTabPanel;
+			var panelId = ParentID ?? "TabControl";
+
+			// If the current container doesn't already contain the tab control, create one now.
+			CustomTabPanel tabControl = container.FindControl(panelId) as CustomTabPanel;
+
 			if (tabControl == null)
 			{
-				tabControl = new CustomTabPanel { ID = "TabControl" };
-				if (container is Component)
-					((Component) container).ContentControls.Add(tabControl);
-				else
-					container.Controls.Add(tabControl);
-			}
+                var controls = container is Component component ? component.ContentControls : container.Controls;
+                tabControl = new CustomTabPanel { ID = panelId };
+
+                controls.Add(tabControl);
+                controls.Add(new LiteralControl("<br />"));
+            }
 
 			Panel tabItem = new Panel
 			{
 				AutoScroll = true,
 				AutoHeight = true,
 				AutoWidth = true,
-				ID = "tabItem" + Name,
+				ID = $"tabItem{panelId}{Name}",
 				Title = Title,
 				BodyStyle = "padding:5px"
 			};
@@ -41,7 +53,46 @@ namespace Zeus.Web.UI
 		}
 	}
 
-	public class CustomTabPanel : TabPanel, INamingContainer
+    public class TabPanelContainerAttribute : EditorContainerAttribute
+    {
+        public string Title { get; set; }
+
+        public bool Collapsible { get; set; }
+
+        public bool Collapsed { get; set; }
+
+        public bool ShowLabel { get; set; }
+
+        public TabPanelContainerAttribute(string name, string title, int sortOrder)
+            : base(name, sortOrder)
+        {
+            Title = title;
+        }
+
+        public override Control AddTo(Control container)
+        {
+            // If the current container doesn't already contain the tab control, create one now.
+            CustomTabPanel tabControl = container.FindControl(Name) as CustomTabPanel;
+
+            if (tabControl == null)
+            {
+                var controls = container is Component component ? component.ContentControls : container.Controls;
+                tabControl = new CustomTabPanel { ID = Name, Title = Title };
+
+                controls.Add(tabControl);
+                controls.Add(new LiteralControl("<br />"));
+            }
+
+            tabControl.Title = Title;
+            tabControl.Collapsible = Collapsible;
+            tabControl.Collapsed = Collapsed;
+            tabControl.FieldLabel = ShowLabel ? Title : "";
+
+            return tabControl;
+        }
+    }
+
+    public class CustomTabPanel : TabPanel, INamingContainer
 	{
 		
 	}
